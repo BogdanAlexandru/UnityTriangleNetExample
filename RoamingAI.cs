a5679d01d323c58b6695b04c8466f735458d6189
Bogdan <bogdanmarginean@ymail.com>
2018-01-31 21:46:52 +0200

    initial commit

---
+++ RoamingAI.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RoamingAI : MonoBehaviour
+{
+    public Area RoamingArea;
+
+    private NavMeshAgent _agent;
+
+    public Transform TargetingSphere;
+
+    public void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+
+        PickNewDestination();
+
+        InvokeRepeating("PickNewDestination", 0f, 3.5f);
+    }
+
+    public bool AtDestination()
+    {
+        if (!_agent.pathPending && _agent.remainingDistance <= _agent.stoppingDistance)
+        {
+            if (!_agent.hasPath || _agent.velocity.sqrMagnitude == 0f)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void Update()
+    {
+        if (!_agent.enabled)
+        {
+            return;
+        }
+    }
+
+    private void PickNewDestination()
+    {
+        NavMeshHit hit;
+        Vector3 pos = transform.position;
+        var loc = RoamingArea.PickRandomLocation();
+        TargetingSphere.transform.position = loc;
+        if (NavMesh.SamplePosition(loc, out hit, 100, 1))
+        {
+            pos = hit.position;
+            _agent.destination = pos;
+            return;
+        }
+        _agent.destination = transform.position;
+    }
+}
